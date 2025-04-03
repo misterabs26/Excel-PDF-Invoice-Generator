c4603495d5e3bd261c011407fa639a8c0dea5773
misterabs26 <mister.abs26@gmail.com>
2025-04-03 18:49:19 +0800

    Added invoice date

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ for excel_file in excel_files:
     df = pd.read_excel(excel_file,sheet_name="Sheet 1")
 
     filename = excel_file.stem
-    invoice_no = filename.split("-")[0]
-    invoice_date = filename.split("-")[1]
+    invoice_no, invoice_date = filename.split("-")
 
     pdf = FPDF(orientation="P",unit="mm",format="letter")
     pdf.add_page()
     pdf.set_font(family="Times", style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice no. {invoice_no}")
+    pdf.cell(w=50, h=8, txt=f"Invoice no.: {invoice_no}", ln=1)
+    pdf.cell(w=50, h=8,txt=f"Date: {invoice_date}", ln=1)
 
 
     pdf.output(f"pdf/{filename}.pdf")
